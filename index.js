acd81f706863504b04ce26158593d6c48fc5e918
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-02-16 22:43:05 +0600

    I output even or odd numbers

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function myName (name) {
 }
 myName("Arsen");
 
+function num(a, b) {
+  for (let i = a; i < b; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " - четный");
+    }
+    else {
+      console.log(i + " - нечетный")
+
+    }
+  }
+}
+num(0,11)
